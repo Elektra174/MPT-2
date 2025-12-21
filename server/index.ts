e08f9fe9d6d178f4eda75a1ce61bd035b596afd0
Elektra174 <elektra-174@yandex.ru>
2025-12-21 20:11:24 +0500

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -76,6 +76,7 @@ app.use((req, res, next) => {
   if (process.env.NODE_ENV === "production") {
     serveStatic(app);
   } else {
+    // Use dynamic import only in development
     const { setupVite } = await import("./vite");
     await setupVite(httpServer, app);
   }
